3779f2786995c569f44f58c82d5a1297c499465a
Leandro Alvarez <leo@Impripost.local>
2021-03-17 18:20:01 -0300

    Se agregaron las primeras rutas de navegacion.

--- fdashboard/src/app/app.module.ts
+++ fdashboard/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
+
+const routes : Routes = [
+  { path: '', redirectTo:'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'suggestion', component: SuggestionsComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SuggestionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing : false }
+    ),
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
