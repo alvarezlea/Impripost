cc0137b673d3b59171b7d8af560411553e761fee
Leandro Alvarez <leo@Impripost.local>
2021-03-23 15:04:53 -0300

    Se realizaron varias modificaciones entre ellas de look

--- fdashboard/src/app/services/card.service.ts
+++ fdashboard/src/app/services/card.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 
 export class CardService {
 
-  multi = [ 
+  tabla = [ 
             [1, "Chat boot IA", "Se trata de un proyecto que se penso con la idea de dar un servicio de chat desantendido.",1], 
             [2, "Desarrollo Web Mobile", "Proyecto para armar un departamento de desarrollo para dispositivos móviles.",2],
             [3, "Desarrollo xx1", "Es simplemente texto de relleno.Es simplemente texto de relleno.Es simplemente texto de relleno.",3],
